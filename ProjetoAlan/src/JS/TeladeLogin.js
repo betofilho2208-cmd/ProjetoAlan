d39e18e1c200f25fce840f9aac509d2b7d7d5081
betofilho2208-cmd <betofilho2208@gmail.com>
2026-06-02 23:06:12 -0300

    alterações no dashboard e tela de login

--- ProjetoAlan/src/JS/TeladeLogin.js
+++ ProjetoAlan/src/JS/TeladeLogin.js
@@ -2,10 +2,25 @@ function login() {
     const email = document.querySelector(".email").value;
     const password = document.querySelector(".password").value;
     const loginButton = document.querySelector(".loginButton");
+    
+     const tipoUsuario = document.querySelector(
+        'input[name="tipoUsuario"]:checked').value;
     // alert("Login realizado com sucesso!");
-    if (email === "admin@gmail.com" && password === "12345") {
-        alert("Login realizado com sucesso!");
+
+   if (email === "admin@gmail.com" && password === "12345") {
+
+        if (tipoUsuario === "Guincheiro") {
+            document.body.classList.add("fade-out");
+                setTimeout(() => {
+
+            window.location.href = "../navigation/Dashboard.html";}, 500);
+
+        } else {
+            alert("Área do cliente ainda não está disponível.");
+        }
+
     } else {
         alert("E-mail ou senha incorretos.");
     }
 }
+
